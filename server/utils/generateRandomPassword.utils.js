81c0da2452c162e514f1ab4c95e24ea9dc306dd5
arpanbht <arpanbht.ofc@gmail.com>
2025-02-24 15:37:11 +0530

    Add user create, login, update password

---
+++ server/utils/generateRandomPassword.utils.js
@@ -0,0 +1,9 @@
+import crypto from "crypto";
+
+export const generateRandomPassword = (length = 8) => {
+  return crypto
+    .randomBytes(Math.ceil(length / 2))
+    .toString("hex")
+    .slice(0, length);
+};
+
