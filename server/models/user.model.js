5957ac9c097bc91828bde25a87a7b05326d27731
N8MARERahul <rahulpal2023iem@gmail.com>
2025-02-24 15:41:32 +0530

    Remove default value for user profile image in user model

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -29,7 +29,6 @@ const userSchema = new Schema(
       type: String,
       default: "https://cdn-icons-png.flaticon.com/512/9131/9131529.png",
       trim: true,
-      default: "",
     },
     department: {
       type: String,
