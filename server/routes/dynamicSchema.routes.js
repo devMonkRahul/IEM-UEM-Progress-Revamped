f91c22a919bc5847d7830c318e240b02059d552f
N8MARERahul <rahulpal2023iem@gmail.com>
2025-03-10 14:40:32 +0530

    Add CRUD routes for dynamic schema management

--- server/routes/dynamicSchema.routes.js
+++ server/routes/dynamicSchema.routes.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
-import { createSchema } from "../controllers/dynamicSchema.controller.js";
+import { createSchema, getAllSchemas, getSchemaById, deleteSchema, updateSchema } from "../controllers/dynamicSchema.controller.js";
 import { verifySuperAdmin } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/createDocument").post(verifySuperAdmin, createSchema);
-
+router.route("/getAllDocuments").post(verifySuperAdmin, getAllSchemas);
+router.route("/getDocument/:schemaId").post(verifySuperAdmin, getSchemaById);
+router.route("/updateDocument/:schemaId").post(verifySuperAdmin, updateSchema);
+router.route("/deleteDocument/:schemaId").post(verifySuperAdmin, deleteSchema);
 
 export default router;
